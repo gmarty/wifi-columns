68ff8a9f229014281d379f6672715f7984b8c15a
Guillaume Marty <edo999@gmail.com>
2015-05-26 15:44:22 +0100

    Add user input for player 1 only

--- app/js/lib/emulatorUi.js
+++ app/js/lib/emulatorUi.js
@@ -29,6 +29,7 @@ class EmulatorUi {
 
     // Screen
     this.screen = ui.querySelector('canvas');
+    var gamepadContainer = ui.querySelector('.gamepad');
 
     this.canvasContext = this.screen.getContext('2d', {'alpha': false});
 
@@ -38,173 +39,43 @@ class EmulatorUi {
     this.canvasImageData = this.canvasContext.getImageData(0, 0, 256, 192);
 
     // Gamepad
-    this.gamepad = {
+    var gamepad = {
       up: 0x01,
-      right: 0x02,
-      down: 0x04,
-      left: 0x08,
+      down: 0x02,
+      left: 0x04,
+      right: 0x08,
       fire1: 0x10,
-      fire2: 0x20
-    };
-    /*var startButton = ui.querSelector('.start');
-
-    // Play button
-    //self.main.start();
-
-    // ROM loader
-    var romFilename = '';
-    var romReader = new FileReader();
-
-    romReader.onload = function(evt) {
-      var data = evt.target.result;
-      self.loadROMFile(data, romFilename);
+      fire2: 0x20,
+      start: 0x40
     };
 
-    $('#romUpload').change(function() {
-      if (document.getElementById('romUpload').files.length === 0) {
-        return;
-      }
-      var oFile = document.getElementById('romUpload').files[0];
-      romFilename = oFile.name;
-      romReader.readAsBinaryString(oFile);
-    });
+    // Software buttons - touch
+    var onTouchStart = evt => {
+      this.main.keyboard.controller1 = 0xFF;
 
-    // ROM selector
-    this.romSelect = $('#romSelector')
-      .change(function() {
-        self.loadROM();
-      });*/
+      var changedTouches = evt.changedTouches;
 
-    // Buttons
-    /*this.buttons.start = $('<button class="btn btn-primary" disabled="disabled">Start</button>')
-     .click(function() {
-     if (!self.main.isRunning) {
-     self.main.start();
-     self.buttons.start.attr('value', 'Pause');
-     } else {
-     self.main.stop();
-     self.updateStatus('Paused');
-     self.buttons.start.attr('value', 'Start');
-     }
-     });*/
+      for (var i = 0; i < changedTouches.length; i++) {
+        var target = document.elementFromPoint(changedTouches[i].clientX, changedTouches[i].clientY);
+        var className = target.className;
 
-    /*this.buttons.reset = $('<button class="btn" disabled="disabled">Reset</button>')
-     .click(function() {
-     if (!self.main.reloadRom()) {
-     $(this).attr('disabled', 'disabled');
-     return;
-     }
-     self.main.reset();
-     self.main.vdp.forceFullRedraw();
-     self.main.start();
-     });*/
-
-    /*if (this.main.soundEnabled) {
-     this.buttons.sound = $('<button class="btn" disabled="disabled">Enable sound</button>')
-     .click(function() {
-     if (self.main.soundEnabled) {
-     self.main.soundEnabled = false;
-     self.buttons.sound.attr('value', 'Enable sound');
-     } else {
-     self.main.soundEnabled = true;
-     self.buttons.sound.attr('value', 'Disable sound');
-     }
-     });
-     }*/
-
-    // @todo Add an exit fullScreen button.
-    /*$('#fullscreen')
-      .click(function() {
-        var screen = */
-    /** @type {HTMLDivElement} */
-    /* (screenContainer[0]);
-
-          if (screen.requestFullscreen) {
-            screen.requestFullscreen();
-          } else {
-            screen.mozRequestFullScreen();
-          }
-        });
-
-      // Software buttons - touch
-      screenContainer.on('touchstart touchmove', function(evt) {
-        self.main.keyboard.controller1 = 0xFF;
-
-        var changedTouches = evt.originalEvent.changedTouches;
-
-        for (var i = 0; i < changedTouches.length; i++) {
-          var target = document.elementFromPoint(changedTouches[i].clientX, changedTouches[i].clientY);
-          var className = target.className;
-
-          if (className === 'gamepad' || !className) {
-            continue;
-          }
-
-          var key = self.gamepad[className];
-          self.main.keyboard.controller1 &= ~key;
+        if (!className || !gamepad[className]) {
+          continue;
         }
 
-        evt.preventDefault();
-      });
-
-      screenContainer.on('touchend', function(evt) {
-        self.main.keyboard.controller1 = 0xFF;
-      });
-
-      // Software buttons - click
-      function mouseDown(evt) {
-        var className = this.className;
-        var key = self.gamepad[className];
-        self.main.keyboard.controller1 &= ~key;
-        evt.preventDefault();
+        this.main.keyboard.controller1 &= ~gamepad[className];
       }
 
-      function mouseUp(evt) {
-        var className = this.className;
-        var key = self.gamepad[className];
-        self.main.keyboard.controller1 |= key;
-        evt.preventDefault();
-      }*/
+      evt.preventDefault();
+    };
 
-    /*for (i in this.gamepad) {
-      var el = screenContainer.querySelector('.' + i);
-      el.mousedown(mouseDown);
-      el.mouseup(mouseUp);
-    }*/
+    var onTouchEnd = () => {
+      this.main.keyboard.controller1 = 0xFF;
+    };
 
-    /*startButton
-      .on('mousedown touchstart', function(evt) {
-        if (self.main.is_sms) {
-          self.main.pause_button = true;       // Pause
-        } else {
-          self.main.keyboard.ggstart &= ~0x80; // Start
-        }
-        evt.preventDefault();
-      })
-      .on('mouseup touchend', function(evt) {
-        if (!self.main.is_sms) {
-          self.main.keyboard.ggstart |= 0x80;  // Start
-        }
-        evt.preventDefault();
-      });*/
-
-    // Keyboard
-    /*$(document)
-      .bind('keydown', function(evt) {
-        self.main.keyboard.keydown(evt);
-        //console.log(self.main.keyboard.controller1, self.main.keyboard.ggstart);
-      })
-      .bind('keyup', function(evt) {
-        self.main.keyboard.keyup(evt);
-        //console.log(self.main.keyboard.controller1, self.main.keyboard.ggstart);
-      });
-
-    this.screen.appendTo(screenContainer);
-    screenContainer.appendTo($(parent));
-
-    if (roms !== undefined) {
-      this.setRoms(roms);
-    }*/
+    gamepadContainer.addEventListener('touchstart', onTouchStart);
+    gamepadContainer.addEventListener('touchmove', onTouchStart);
+    gamepadContainer.addEventListener('touchend', onTouchEnd);
 
     document.querySelector('#host div.emulator').appendChild(ui);
 
@@ -234,8 +105,6 @@ class EmulatorUi {
     this.main.vdp.forceFullRedraw();
     this.enable();
 
-    //$('#play').removeClass('disabled');
-    //$('#fullscreen').removeClass('disabled');
     this.main.start();
   }
 
